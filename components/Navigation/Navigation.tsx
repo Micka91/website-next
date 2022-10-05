b0926764f866382cb99cc537968057d921eb6f39
LAPTOP-I19I4EUE\Micka <mickael.christine@hotmail.com>
2022-10-05 18:08:54 +0200

    add link for blog

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -93,7 +93,15 @@ const Navigation = () => {
               </Link>
             </li>
           ))}
-
+          <li className="navigation__item">
+            <a
+              href="https://blog.teamdoc.fr/blog/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BLOG
+            </a>
+          </li>
           {/* SWITCHLANG */}
           <li
             className="navigation__item navigation__item--lang"
